a4f79489b19769a7adae2739c16c0bfe34a6b0a0
Cayo Roberto <cayoroberto08@gmail.com>
2025-02-27 10:21:54 -0300

    feat: configuracoes de comunicacao http

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { meuhttpInterceptor } from '../app/services/http-inteceptor.service';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([meuhttpInterceptor])
-  )]
+    provideHttpClient(withFetch())
+  ]
 };
